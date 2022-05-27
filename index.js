77d981c4d30c9e9f83bda9939450d1eecd7d14c3
Mustafa Aydogan <mm.k.aydogan@gmail.com>
2022-05-28 01:30:40 +0300

    fib

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+function fib(n) {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+
 app.get('/', (req, res) => {
-    res.send('ДИМАН ПЕТУХ!')
+    res.send(fib(1000))
 })
 
 app.listen(port, () => {
