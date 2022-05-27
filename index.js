91bda244cfe73707be3c3f11dc55967c4d610a3d
am.mussy <aydoganwork@gmail.com>
2022-05-28 02:48:08 +0300

    Kriss

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-function fib(n) {
-  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
-}
-
 app.get('/', (req, res) => {
-    res.send(fib(1))
+    res.send('Кристинка, - любимка')
 })
 
 app.listen(port, () => {
