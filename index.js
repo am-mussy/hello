656c6d6b874f8110a00d574accef9d39ee87c721
am.mussy <aydoganwork@gmail.com>
2022-05-28 23:29:41 +0300

    add docker file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Кристинка, - любимка')
+    res.send('ДИМОООООООООООООООН, ходят соухи, что он ПЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕТУХ')
 })
 
 app.listen(port, () => {
